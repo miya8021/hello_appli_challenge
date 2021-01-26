342b9879f29bfbbad32c2b47060a82639767e212
miya <rinn.sasami@gmail.com>
2021-01-26 15:03:40 +0900

    change user to home

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,10 @@
-class UsersController < ApplicationController
+class HomesController < ApplicationController
   def index
     @message = "Hello World"
     user = User.new
     @my_introduce = user.introduce
   end
 end
+
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
- get "/users", to: "users#index"
+ get "/homes", to: "homes#index"
 end
 
